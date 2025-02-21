2e8a6704275fba8c6b6934b108cf7fd0b94a4958
Guilherme Oliveira <guioliveira99@gmail.com>
2025-02-20 22:28:59 -0300

    Adding enemy logic to limit chase

---
+++ Source/Invasion/Private/SpecificAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SpecificAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASpecificAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
